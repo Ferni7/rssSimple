1a00f9b1684f67bafe0aac970d91b6a3d3ed7703
andrew <fernii@gmail.com>
2019-12-01 03:29:57 +0000

    Adding Dockerfile and supressed php warnings

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?
+error_reporting(E_ERROR | E_PARSE);
 header('Content-type: application/rss+xml');
 
 $feed = implode(file($_GET['feed']));
-
 $xml = simplexml_load_string($feed);
 $json = json_encode($xml);
 $array = json_decode($json,TRUE);
-echo "<?xml version=\"1.0\"?>\n\r"
+echo "<?xml version=\"1.0\"?>\r\n";
 ?>
 <rss version="2.0">
   <channel>
 <?
-echo "\t<title>" . htmlspecialchars($array[channel][title]) . "</title>\r\n";
-echo "\t<link>" . htmlspecialchars($array[channel][link]) . "</link>\r\n";
-echo "\t<description>" . htmlspecialchars($array[channel][description]) . "</description>\r\n";
+echo "\t<title>" . htmlspecialchars($array['channel']['title']) . "</title>\r\n";
+echo "\t<link>" . htmlspecialchars($array['channel']['link']) . "</link>\r\n";
+echo "\t<description>" . htmlspecialchars($array['channel']['description']) . "</description>\r\n";
 
-foreach ( $array[channel][item] as $item ) {
+foreach ( $array['channel']['item'] as $item ) {
   echo "\t<item>\r\n";
-  echo "\t\t<title>" . htmlspecialchars($item[title]) . "</title>\r\n";
-  echo "\t\t<link>" . htmlspecialchars($item[link]) . "</link>\r\n";
-  echo "\t\t<pubDate>" . htmlspecialchars($item[pubDate]) . "</pubDate>\r\n";
+  echo "\t\t<title>" . htmlspecialchars($item['title']) . "</title>\r\n";
+  echo "\t\t<link>" . htmlspecialchars($item['link']) . "</link>\r\n";
+  echo "\t\t<pubDate>" . htmlspecialchars($item['pubDate']) . "</pubDate>\r\n";
   echo "\t</item>\r\n";
 }
 ?>
